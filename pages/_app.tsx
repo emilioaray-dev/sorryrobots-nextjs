ffce2432341a1df020522e33c62b9ce6aea9e226
Celsius Aray <emilioaray@gmail.com>
2021-11-30 23:56:22 -0400

    First Commit Header

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,6 @@
 import '../styles/globals.css'
+import '../styles/sorry.css'
+import '../styles/webflow.css'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
